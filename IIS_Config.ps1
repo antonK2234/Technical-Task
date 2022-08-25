407ba81c913f780e36fd6758389fe233f86ec675
antonK2234 <91905550+antonK2234@users.noreply.github.com>
2022-08-25 17:38:18 +0300

    Update IIS_Config.ps1

--- IIS_Config.ps1
+++ IIS_Config.ps1
@@ -1,3 +1,14 @@
 Install-WindowsFeature -name Web-Server -IncludeManagementTools
-Remove-Item  C:\inetpub\wwwroot\iisstart.htm
-Add-Content -Path "C:\inetpub\wwwroot\iisstart.htm" -Value $("Hello evrybody from " + $env:computername)
+Remove-Item  C:\inetpub\wwwroot\iisstart.html
+New-Item -ItemType File -Name 'index.html' -Path 'C:\inetpub\wwwroot\'
+Add-Content -Path "C:\inetpub\wwwroot\index.html" -Value $("
+<!DOCTYPE html>
+<html>
+    <head>
+         <title>IIS MyWebsite Demo</title>
+    </head>
+    <body>
+        <h1>IIS MyWebsite Demo</h1>
+        <h2>Hello evrybody from + $env:computername </h2>
+    </body>
+</html> ")
